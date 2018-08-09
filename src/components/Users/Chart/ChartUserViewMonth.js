2c6ee01798c023dbc2db93ab7b2e995773494e4c
waranchit.ch@gmail.com <waranchit.ch@gmail.com>
2018-08-10 00:44:33 +0700

    add Progress in UserView Month

--- src/components/Users/Chart/ChartUserViewMonth.js
+++ src/components/Users/Chart/ChartUserViewMonth.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {
   Grid,
-  Paper
+  Paper,
+  CircularProgress
 } from "@material-ui/core";
+import '../../../styles/user.css';
 import Loadable from "react-loadable";
 
 
@@ -27,7 +29,7 @@ const SelectYear = Loadable({
   loading: () => null
 });
 const SelectYearAndMonth = Loadable({
-  loader: () => import('../SelectChange/ChangeYearAndMonth'),
+  loader: () => import("../SelectChange/ChangeYearAndMonth"),
   loading: () => null
 });
 
@@ -39,25 +41,31 @@ const USerView = ({
   submitSelectYear
 }) => (
   <div>
-      <div>
-        <Grid container justify='center'>
-          <Grid item sm={7} xs={12}>
-            <SelectYear valueYear={valueYear} year={year} selectYear={selectYear} submitSelectYear={submitSelectYear}/>
-            <Paper elevation={5}>
-              <TitleChart year={year}/>
-              <ChartForMonth
-                dataChartMonth={dataChartMonth}/>
-            </Paper>
-          </Grid>
-        </Grid>
-        <Grid container justify='center' className='paperShowResult'>
-          <Grid item sm={5} xs={12}>
-            <CardResultYear
-              year={year}
-              dataMonthChart={dataChartMonth}/>
-          </Grid>
-        </Grid>
-      </div>
+      {
+        dataChartMonth === null ? (<CircularProgress size={250} thickness={7} className='centeredProgress'/>) :
+          (
+            <div>
+              <Grid container justify='center'>
+                <Grid item sm={7} xs={12}>
+                  <SelectYear valueYear={valueYear} year={year} selectYear={selectYear}
+                              submitSelectYear={submitSelectYear}/>
+                  <Paper elevation={5}>
+                    <TitleChart year={year}/>
+                    <ChartForMonth
+                      dataChartMonth={dataChartMonth}/>
+                  </Paper>
+                </Grid>
+              </Grid>
+              <Grid container justify='center' className='paperShowResult'>
+                <Grid item sm={5} xs={12}>
+                  <CardResultYear
+                    year={year}
+                    dataMonthChart={dataChartMonth}/>
+                </Grid>
+              </Grid>
+            </div>
+          )
+      }
   </div>
 );
 
